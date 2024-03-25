9f07cbe7c93546a659bfe8a2dc6a902161aaee22
kat-ki <glraskat@gmail.com>
2024-03-25 14:38:24 +0100

    fix: 4.1-4.2 added middleware

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogsRoutes')
 const {MONGODB_URI} = require('./utils/config')
 const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 
 mongoose.set('strictQuery', false)
 
@@ -20,7 +21,11 @@ mongoose.connect(MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
