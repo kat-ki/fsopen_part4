823d4009f40b2864027a982d089a3cf59b057de1
kat-ki <glraskat@gmail.com>
2024-03-25 14:08:42 +0100

    fix: 4.1-4.2 refactored

--- app.js
+++ app.js
@@ -4,9 +4,19 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const blogRouter = require('./controllers/blogRouter')
 const {MONGODB_URI, PORT} = require('./utils/config')
+const logger = require('./utils/logger')
 
-const mongoUrl = MONGODB_URI
-mongoose.connect(mongoUrl)
+mongoose.set('strictQuery', false)
+
+logger.info('connecting to', MONGODB_URI)
+
+mongoose.connect(MONGODB_URI)
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch(error => {
+        logger.error('error connecting to MongoDB', error.message)
+    })
 
 app.use(cors())
 app.use(express.json())
