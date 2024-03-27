2684fb9f10820731cad345730163660673a68e22
kat-ki <glraskat@gmail.com>
2024-03-27 12:48:50 +0100

    feat: 4.6 added getTopAuthor function and test

--- tests/topAuthor.test.js
+++ tests/topAuthor.test.js
@@ -2,7 +2,7 @@ const {test, describe} = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helpers')
 
-describe('total likes', () => {
+describe('top author', () => {
     const blogs = [
         {
             _id: "5a422a851b54a676234d17f7",
@@ -53,31 +53,12 @@ describe('total likes', () => {
             __v: 0
         }
     ]
-    test('total sum of likes in all blogs', () => {
-        const result = listHelper.getTotalLikes(blogs)
-        assert.strictEqual(result, 41)
-    })
-
-    test('blog with most likes', () => {
-        const result = listHelper.getFavoriteBlog(blogs)
+    test('top author with most blogs', () => {
+        const result = listHelper.getTopAuthor(blogs)
         console.log(result)
-        assert.strictEqual(result.likes, 12)
-    })
-
-
-    const listWithOneBlog = [
-        {
-            _id: '5a422aa71b54a676234d17f8',
-            title: 'Go To Statement Considered Harmful',
-            author: 'Edsger W. Dijkstra',
-            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-            likes: 5,
-            __v: 0
-        }
-    ]
-    test('when list has only one blog, equals the likes of that', () => {
-        const result = listHelper.getTotalLikes(listWithOneBlog)
-        assert.strictEqual(result, 5)
+        assert.deepStrictEqual(result, {
+            author: "Robert C. Martin",
+            blogs: 3
+        })
     })
 })
-
--- utils/list_helpers.js
+++ utils/list_helpers.js
@@ -9,15 +9,37 @@ const getTotalLikes = (blogs) => {
 const getFavoriteBlog = (blogs) => {
     let blogWithMostLikes = {}
     let mostLikes = 0;
+
     for (let i = 0; i < blogs.length; i++) {
-        if(blogs[i].likes > mostLikes) {
+        if (blogs[i].likes > mostLikes) {
             mostLikes = blogs[i].likes
             blogWithMostLikes = blogs[i]
         }
     }
+
     return blogWithMostLikes;
 }
 
+const getTopAuthor = (blogs) => {
+    let authors = {}
+    let topAuthor = {author: '', blogs: 0}
 
-module.exports = {dummy, getTotalLikes, getFavoriteBlog}
+    for (let i = 0; i < blogs.length; i++) {
+        let currAuthorName = blogs[i].author;
+        if (!authors[currAuthorName]) {
+            authors[currAuthorName] = 1;
+        } else {
+            authors[currAuthorName]++;
+        }
+
+        if (authors[currAuthorName] > topAuthor.blogs) {
+            topAuthor.author = currAuthorName;
+            topAuthor.blogs = authors[currAuthorName];
+        }
+    }
+
+    return topAuthor;
+}
+
+module.exports = {dummy, getTotalLikes, getFavoriteBlog, getTopAuthor}
 
