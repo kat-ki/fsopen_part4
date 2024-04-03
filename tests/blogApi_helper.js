2eda8c96ecd2e45294349bf45787b6381fe5f4b4
kat-ki <glraskat@gmail.com>
2024-04-03 12:19:40 +0200

    4.8: added tests for GET in json format and correct amount of returned items

---
+++ tests/blogApi_helper.js
@@ -0,0 +1,18 @@
+const Blog = require('../models/blog')
+
+const initBlogs = [
+    {
+        "title": "Better test earlier than later",
+        "author": "John Smith",
+        "url": "",
+        "likes": 5
+    },
+    {
+        "title": "Work smarter not harder",
+        "author": "Abra Kadabra",
+        "url": "",
+        "likes": 8
+    }
+]
+
+module.exports = initBlogs
---
+++ tests/blog_api.test.js
@@ -0,0 +1,34 @@
+const {test, beforeEach, after} = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+
+const app = require('../app')
+const Blog = require('../models/blog')
+const initBlogs = require('./blogApi_helper')
+const api = supertest(app)
+
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+
+    let blogElements = initBlogs.map(blog => new Blog(blog))
+    const blogElementPromises = blogElements.map(blog => blog.save())
+
+    await Promise.all(blogElementPromises)
+})
+test('blog posts are returned in JSON format', async () => {
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+})
+test('correct amount of blog posts is returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    assert.strictEqual(response.body.length, initBlogs.length)
+})
+
+after(async () => {
+    await mongoose.connection.close()
+})
